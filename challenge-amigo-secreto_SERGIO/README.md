b7f2990d4da071033345bfdd2bd736a2339ca657
vortsl <77473722+vortsl@users.noreply.github.com>
2025-03-18 18:55:04 -0600

    Add files via upload

---
+++ challenge-amigo-secreto_SERGIO/README.md
@@ -0,0 +1,4 @@
+# Challenge Alura - Amigo Secreto
+
+Este desafío consiste en una aplicación que permite a los usuarios ingresar nombres de amigos en una lista, realizar un sorteo aleatorio y determinar quién es el "Amigo Secreto"
+
---
+++ challenge-amigo-secreto_SERGIO/app.js
@@ -0,0 +1,41 @@
+let amigos = [];
+
+
+function agregarAmigo() {
+  let nombreAmigo = document.getElementById("amigo").value;
+
+  if (nombreAmigo.trim() === "") {
+    alert("Por favor, inserte un nombre");
+  } else {
+    amigos.push(nombreAmigo);
+    document.querySelector("#amigo").value = "";
+    mostrarListaAmigo();
+  }
+}
+
+
+function mostrarListaAmigo() {
+  let listaAmigos = document.querySelector("#listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  for (let index = 0; index < amigos.length; index++) {
+    const element = amigos[index];
+
+    let listaHTML = document.createElement("li");
+    listaHTML.textContent = element;
+    listaAmigos.appendChild(listaHTML);
+  }
+}
+
+
+
+function sortearAmigo() {
+  let cantidadAmigos = amigos.length;
+  if (cantidadAmigos === 0) {
+    alert("Por favor, inserte un nombre antes de sortear");
+  } else {
+    let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+    let resultadoHTML = document.querySelector("#resultado");
+    resultadoHTML.innerHTML = amigos[indiceAmigo];
+  }
+}
